9d53bfce89d2b13372afe60521a8d7a9bcc931de
Sergio Gonzales Estenssoro <sergio.estenssoro@gmail.com>
2022-03-24 20:33:28 -0400

    Modificacion en titulo de ListadoPacientes.jsx

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -4,7 +4,7 @@ const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
       {pacientes && pacientes.length ?     <>
     <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-    <p className="text-xl mt-5 mb-10 text-center"> Administra tus {""} <span className="text-indigo-600 font-bold">Pacientes y Citas</span></p>
+    <p className="text-xl mt-5 mb-10 text-center"> Administra tus {""} <span className="text-indigo-600 font-bold">Pacientes y Citas Aquí</span></p>
         {
           pacientes.map((paciente) => (
             <Paciente
